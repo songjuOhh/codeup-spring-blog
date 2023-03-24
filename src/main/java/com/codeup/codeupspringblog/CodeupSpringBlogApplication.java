3bc9acb937eead4deb1e9dbfb13c3d0b00e5551c
Songju Oh <songju.oh1@gmail.com>
2023-03-24 10:37:52 -0500

    spring boot start

---
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -0,0 +1,15 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringBlogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CodeupSpringBlogApplication.class, args);
+
+	}
+
+}
